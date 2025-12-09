2ff33c55e8a2b185285dd3feef46b2223be4a62a
yakup <yakup.6791058@gmail.com>
2025-12-09 19:49:50 +0300

    ssl problem fix

--- src/config/database.js
+++ src/config/database.js
@@ -9,6 +9,12 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT || 5432,
     dialect: 'postgres',
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    },
     pool: {
       max: 5,
       min: 0,
